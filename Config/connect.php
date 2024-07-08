a59f9d95e53a94fed3472cff59aebd494591c3f0
tng1804 <tranvanngoc180403@gmail.com>
2024-07-08 11:07:30 -0700

    add utf8

--- Config/connect.php
+++ Config/connect.php
@@ -2,7 +2,7 @@
     $conn = mysqli_connect("localhost", "root", "admin", "vanphongpham");
     if(!$conn){
 	
-	// Set charset to UTF-8
+	// Set charset to UTF-8 cho connect
 	$conn->set_charset("utf8mb4");
         die("Ket noi khong thanh cong");
         exit();
